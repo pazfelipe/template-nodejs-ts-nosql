3ef36bc558147290e628da7e2e991128d2e8d13e
Felipe Paz <>
2021-09-05 22:18:12 -0300

    feat: adicionado middleware de erro

--- app/source/app.ts
+++ app/source/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import router from './routes';
+import NextError from './utils/nextError';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+app.use(NextError);
 
 const server = http.createServer(app);
 
